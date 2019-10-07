4750a3ecd8608e2b6e9d91531799c8bd53ec5c5f
Suraj <surajpatel527@gmail.com>
2019-10-06 21:35:12 -0400

    Changelog: Optimized code in Snake.js

--- snake/source/Snake.js
+++ snake/source/Snake.js
@@ -12,33 +12,36 @@ class Snake {
      * @param {?number} dirLeft
      */
     constructor(board, matrix, links, dirTop, dirLeft) {
-        this.board      = board;
-        this.matrix     = matrix;
-        
-        this.container  = document.querySelector(".snake");
-        this.position   = Utils.getPosition(this.container);
-        this.queue      = new Queue();
-        this.dirTop     = dirTop  !== undefined ? dirTop  : 1;
-        this.dirLeft    = dirLeft !== undefined ? dirLeft : 0;
-        this.newDir     = false;
-        this.initialPos = { top : 3, left : 11 };
-        
+        this.board = board;
+        this.matrix = matrix;
+
+        this.container = document.querySelector(".snake");
+        this.position = Utils.getPosition(this.container);
+        this.queue = new Queue();
+        this.dirTop = dirTop !== undefined ? dirTop : 1;
+        this.dirLeft = dirLeft !== undefined ? dirLeft : 0;
+        this.newDir = false;
+        this.initialPos = {
+            top: 3,
+            left: 11
+        };
+
         this.container.innerHTML = "";
-        
+
         if (links) {
             links.forEach((link) => {
                 this.addLink(this.board.createSnakeElement(), link.top, link.left);
             });
         }
     }
-    
+
     /**
      * Moves the snake
      * @return {string} The result of the movement
      */
     move() {
         let pos = this.getPosition();
-        
+
         if (this.matrix.crashed(pos.top, pos.left)) {
             return "crashed";
         }
@@ -52,10 +55,10 @@ class Snake {
             this.moveLink(pos.top, pos.left);
         }
         this.newDir = false;
-        
+
         return "";
     }
-    
+
     /**
      * Creates a new Element to the snake
      * @param {number} top
@@ -65,7 +68,7 @@ class Snake {
         this.addLink(this.board.createSnakeElement(), top, left);
         this.matrix.addSnake(top, left);
     }
-    
+
     /**
      * Move the last link to head of the snake
      * @param {number} top
@@ -74,11 +77,11 @@ class Snake {
     moveLink(top, left) {
         let first = this.queue.dequeue();
         this.addLink(first.element, top, left);
-        
+
         this.matrix.removeSnake(first.top, first.left);
         this.matrix.addSnake(top, left);
     }
-    
+
     /**
      * Adds a link to the head of the snake
      * @param {DOMElement} element
@@ -86,18 +89,18 @@ class Snake {
      * @param {number}     left
      */
     addLink(element, top, left) {
-        element.style.top  = this.board.getPosition(top);
+        element.style.top = this.board.getPosition(top);
         element.style.left = this.board.getPosition(left);
-        
+
         this.queue.enqueue({
-            element : element,
-            top     : top,
-            left    : left
+            element: element,
+            top: top,
+            left: left
         });
         this.container.appendChild(element);
     }
-    
-    
+
+
     /**
      * Change the direction of the snake
      * @param {number} dirTop
@@ -106,14 +109,14 @@ class Snake {
      */
     turn(dirTop, dirLeft) {
         if (((!this.dirTop && dirTop) || (!this.dirLeft && dirLeft)) && !this.newDir) {
-            this.dirTop  = dirTop;
+            this.dirTop = dirTop;
             this.dirLeft = dirLeft;
-            this.newDir  = true;
+            this.newDir = true;
             return true;
         }
         return false;
     }
-    
+
     /**
      * Turns the snake using the mouse
      * @param {Event} event
@@ -121,14 +124,14 @@ class Snake {
      */
     mouseTurn(event) {
         let mouse = Utils.getMousePos(event),
-            last  = this.queue.last(),
-            cell  = this.board.cellSize,
-            top   = Math.floor((mouse.top  - this.position.top)  / cell),
-            left  = Math.floor((mouse.left - this.position.left) / cell),
-            dtop  = top  - last.top,
+            last = this.queue.last(),
+            cell = this.board.cellSize,
+            top = Math.floor((mouse.top - this.position.top) / cell),
+            left = Math.floor((mouse.left - this.position.left) / cell),
+            dtop = top - last.top,
             dleft = left - last.left,
-            can   = false;
-        
+            can = false;
+
         if (Math.abs(dtop) > Math.abs(dleft)) {
             can = this.turn(dtop < 0 ? -1 : 1, 0);
             if (!can) {
@@ -142,8 +145,8 @@ class Snake {
         }
         return can;
     }
-        
-    
+
+
     /**
      * Returns the next position of the last element in the queue
      * @return {{top: number, left: nuber}}
@@ -152,19 +155,22 @@ class Snake {
         if (this.queue.isEmpty()) {
             return this.initialPos;
         }
-        
+
         let last = this.queue.last();
         return {
-            top  : last.top  + this.dirTop,
-            left : last.left + this.dirLeft
+            top: last.top + this.dirTop,
+            left: last.left + this.dirLeft
         };
     }
-    
+
     /**
      * Returns the current direction of the snake
      * @return {{top: number, left: nuber}}
      */
     getDirection() {
-        return { top : this.dirTop, left : this.dirLeft };
+        return {
+            top: this.dirTop,
+            left: this.dirLeft
+        };
     }
 }
