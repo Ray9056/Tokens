b4e3d5625a47e68875270c03a64abb002d68d90f
Suraj Patel <Surajpatel527@gmail.com>
2019-10-19 13:24:15 -0400

    Changelog: Main game canvas class - Start constructor

---
+++ pacman/source/board/GameCanvas.js
@@ -0,0 +1,13 @@
+/**
+ * @extends {Canvas}
+ * The Game Canvas Class
+ */
+class GameCanvas extends Canvas {
+    
+    /**
+     * The Game Canvas constructor
+     */
+    constructor() {
+        super();
+        this.init("game");
+    }
