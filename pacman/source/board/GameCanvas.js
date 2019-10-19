b738a74e62d09c8f63dbb983e39fb1a591eaf23e
Suraj Patel <Surajpatel527@gmail.com>
2019-10-19 13:25:24 -0400

    Changelog: Created ghosts for testing

--- pacman/source/board/GameCanvas.js
+++ pacman/source/board/GameCanvas.js
@@ -11,3 +11,24 @@ class GameCanvas extends Canvas {
         super();
         this.init("game");
     }
+
+/**
+     * Draws the Ghosts Targets for testing
+     * @param {Array.<Ghost>} ghosts
+     */
+    drawTargets(ghosts) {
+        this.ctx.save();
+        ghosts.forEach((ghost) => {
+            this.ctx.fillStyle   = ghost.getBodyColor();
+            this.ctx.strokeStyle = ghost.getBodyColor();
+            
+            let tile = Board.getTileXYCenter(ghost.getTargetTile());
+            this.ctx.beginPath();
+            this.ctx.moveTo(ghost.getX(), ghost.getY());
+            this.ctx.lineTo(tile.x, tile.y);
+            this.ctx.fillRect(tile.x - 4, tile.y - 4, 8, 8);
+            this.ctx.stroke();
+        });
+        this.ctx.restore();
+    }
+}
