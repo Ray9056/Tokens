cb87095ac384de744e223b0d4a36d581daa2bdea
Suraj Patel <Surajpatel527@gmail.com>
2019-10-20 13:16:17 -0400

    Changelog: Starting main brain of snake -- Added Game Over and blob eatting

---
+++ pacman/source/Init.js
@@ -0,0 +1,81 @@
+(function () {
+    "use strict";
+    
+    var display, demo, animations, sounds, scores,
+        score, food, fruit, ghosts, blob,
+        animation, startTime, actions, shortcuts,
+        soundFiles  = [ "start", "death", "eat1", "eat2", "kill" ],
+        specialKeys = {
+            "8"  : "BS",
+            "13" : "Enter",
+            "37" : "Left",
+            "65" : "Left",
+            "38" : "Up",
+            "87" : "Up",
+            "39" : "Right",
+            "68" : "Right",
+            "40" : "Down",
+            "83" : "Down"
+        };
+    
+    
+    
+    /**
+     * Calls the Game Over animation and then deletes the game data
+     */
+    function gameOver() {
+        display.set("ready");
+        animations.gameOver(() => {
+            food   = null;
+            fruit  = null;
+            ghosts = null;
+            blob   = null;
+            
+            Board.clearAll();
+            display.set("gameOver").show();
+            scores.setInput();
+        });
+    }
+    
+    /**
+     * Creates the Blob and the Ghosts, and starts the Ready animation
+     * @param {boolean} newLife
+     */
+    function createPlayers(newLife) {
+        ghosts = new Ghosts(newLife ? ghosts : null);
+        blob   = new Blob();
+        
+        blob.draw();
+        ghosts.draw();
+        animations.ready(() => display.set("playing"));
+    }
+    
+    
+    /**
+     * Called when the Blob enters a new tile
+     */
+    function blobEating() {
+        let tile   = blob.getTile(),
+            atPill = food.isAtPill(tile);
+        
+        if (atPill) {
+            let value = food.eatPill(tile),
+                total = food.getLeftPills();
+            
+            fruit.add(total);
+            score.pill(value);
+            ghosts.resetPenTimer();
+            ghosts.checkElroyDots(total);
+            
+            if (value === Data.energizerValue) {
+                ghosts.frighten(blob);
+            }
+            sounds[blob.getSound()]();
+        
+        } else if (fruit.isAtPos(tile)) {
+            let text = score.fruit();
+            fruit.eat();
+            animations.fruitScore(text, Board.fruitTile);
+        }
+        blob.onEat(atPill, ghosts.areFrighten());
+    }
