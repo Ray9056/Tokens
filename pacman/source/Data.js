373cbf883c1e788c590947509faf67f8a39f3419
Suraj Patel <Surajpatel527@gmail.com>
2019-10-25 13:38:00 -0400

    Changelog: Seventh array of data with comments

--- pacman/source/Data.js
+++ pacman/source/Data.js
@@ -121,6 +121,26 @@ let Data = (function (){
             switchTimes       : [ 5, 20, 5, 20, 5, 1037, 1 / 60, 1 ],
             penForceTime      : 3,
             penLeavingLimit   : [ 0, 0, 0, 0 ]
-        }
+        },
+        { // 7
+            ghostSpeed        : 0.95,
+            tunnelSpeed       : 0.5,
+            pmSpeed           : 1,
+            eatingSpeed       : 0.87,
+            ghostFrightSpeed  : 0.6,
+            pmFrightSpeed     : 1,
+            eatingFrightSpeed : 0.87,
+            elroyDotsLeft1    : 50,
+            elroySpeed1       : 1,
+            elroyDotsLeft2    : 25,
+            elroySpeed2       : 1.05,
+            fruitType         : 5,
+            fruitScore        : 1000,
+            frightTime        : 2,
+            frightBlinks      : 5,
+            switchTimes       : [ 5, 20, 5, 20, 5, 1037, 1 / 60, 1 ],
+            penForceTime      : 3,
+            penLeavingLimit   : [ 0, 0, 0, 0 ]
+        },
     ]
 }());
