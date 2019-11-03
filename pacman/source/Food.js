15323b7841a8b4d8bfa8bbda80a70cf576fa27c7
Suraj Patel <Surajpatel527@gmail.com>
2019-11-03 13:09:37 -0500

    Changelog: Creates a Matrix with the positions of pills and energizers

--- pacman/source/Food.js
+++ pacman/source/Food.js
@@ -26,5 +26,21 @@ class Food {
         this.matrix     = [];
         this.mult       = -1;
     }
+
+    /**
+     * Creates a Matrix with the positions of the pills and energizers
+     */
+    createMatrix() {
+        for (let i = 0; i < Board.rows; i += 1) {
+            this.matrix[i] = [];
+            for (let j = 0; j < Board.cols; j += 1) {
+                this.matrix[i][j] = Board.hasPill(j, i) ? Data.pillValue : 0;
+            }
+        }
+        
+        Board.energizers.forEach((pos) => {
+            this.matrix[pos.y][pos.x] = Data.energizerValue;
+        });
+    }
     
 }
