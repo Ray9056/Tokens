0340dfa227bf494b34cc81b5208b9ceb9c604095
Suraj Patel <Surajpatel527@gmail.com>
2019-11-05 13:17:19 -0500

    Changelog: Started Fruit class/constructor

---
+++ pacman/source/Fruit.js
@@ -0,0 +1,14 @@
+/**
+ * The Fruit Class
+ */
+class Fruit {
+    
+    /**
+     * The Fruit constructor
+     */
+    constructor() {
+        this.ctx   = Board.boardCanvas.context;
+        this.timer = 0;
+    }
+
+}
