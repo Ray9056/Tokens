fc74182bd0af01735ffcbd9db00c449472e718a2
Suraj Patel <Surajpatel527@gmail.com>
2019-11-07 13:37:54 -0500

    Changelog: Draws the Cherries Fruit

--- pacman/source/Fruit.js
+++ pacman/source/Fruit.js
@@ -80,6 +80,40 @@ class Fruit {
         let pos = Board.fruitPos;
         this.ctx.clearRect(pos.x - 1, pos.y - 1, Board.fruitSize, Board.fruitSize);
     }
+
+    /**
+     * Draws the Cherries Fruit
+     */
+    drawCherries() {
+        this.ctx.fillStyle = "rgb(255, 0, 0)";
+        this.ctx.beginPath();
+        this.ctx.arc(10, 14, 4, 0, 2 * Math.PI);
+        this.ctx.arc(4, 10, 4, 0, 2 * Math.PI);
+        this.ctx.fill();
+        
+        this.ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+        this.ctx.beginPath();
+        this.ctx.arc(8, 15.5, 1.5, 0, 2 * Math.PI);
+        this.ctx.arc(1.5, 11, 1.5, 0, 2 * Math.PI);
+        this.ctx.fill();
+        
+        this.ctx.strokeStyle = "rgb(0, 153, 0)";
+        this.ctx.lineWidth = 2;
+        this.ctx.beginPath();
+        this.ctx.moveTo(17, 1);
+        this.ctx.quadraticCurveTo(9, 1, 5, 9);
+        this.ctx.moveTo(17, 1);
+        this.ctx.quadraticCurveTo(12, 3, 10, 12);
+        this.ctx.stroke();
+        
+        this.ctx.strokeStyle = "rgb(222, 151, 81)";
+        this.ctx.lineWidth = 3;
+        this.ctx.lineCap = "round";
+        this.ctx.beginPath();
+        this.ctx.moveTo(17, 1);
+        this.ctx.lineTo(16, 2);
+        this.ctx.stroke();
+    }
     
 
 }
