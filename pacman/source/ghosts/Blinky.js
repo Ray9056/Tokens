a84c010c02bc1cd24a91fa7b735fbfd1a37767c1
Suraj Patel <Surajpatel527@gmail.com>
2019-11-17 14:02:52 -0500

    Changelog: Started Blinky class/constructor git push

---
+++ pacman/source/ghosts/Blinky.js
@@ -0,0 +1,37 @@
+/**
+ * @extends {Ghost}
+ * The Blinky Class
+ */
+class Blinky extends Ghost {
+    
+    /**
+     * The Blinky constructor
+     * @param {Canvas}  canvas
+     * @param {?number} dots
+     */
+    constructor(canvas, dots) {
+        super();
+        
+        this.paths = {
+            exitPen : [
+                { dir : { x:  0, y: -1 }, disty : 138, next : null }
+            ],
+            enterPen : [
+                { dir : { x: -1, y:  0 }, distx : 168, next : 1    },
+                { dir : { x:  0, y:  1 }, disty : 174, next : null }
+            ]
+        };
+        
+        this.id      = 0;
+        this.x       = 168;
+        this.y       = 138;
+        this.dir     = { x: -1, y:  0 };
+        this.scatter = { x: 25, y: -3 };
+        this.inPen   = false;
+        this.color   = Blinky.color;
+        
+        this.init(canvas, dots);
+        
+        this.elroyMode   = 0;
+        this.activeElroy = dots !== null;
+    }
