a48397de76f1160be6ae37663b93871ebceed4a4
SurajPatel <Surajpatel527@gmail.com>
2022-11-17 19:47:31 -0500

    update constant
    keys, points, rotation, and calls

--- tetris/constant.js
+++ tetris/constant.js
@@ -29,3 +29,54 @@ const SHAPES = [
     [[7, 7, 0], [0, 7, 7], [0, 0, 0]]
 ];
 
+const KEY = {
+    ESC: 27,
+    SPACE: 32,
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40,
+    P: 80,
+    Q: 81
+};
+
+const POINTS = {
+    SINGLE: 100,
+    DOUBLE: 300,
+    TRIPLE: 500,
+    TETRIS: 800,
+    SOFT_DROP: 1,
+    HARD_DROP: 2,
+};
+
+const LEVEL = {
+    0: 800,
+    1: 720,
+    2: 630,
+    3: 550,
+    4: 470,
+    5: 380,
+    6: 300,
+    7: 220,
+    8: 130,
+    9: 100,
+    10: 80,
+    11: 80,
+    12: 80,
+    13: 70,
+    14: 70,
+    15: 70,
+    16: 50,
+    17: 50,
+    18: 50,
+    19: 30,
+    20: 30,
+    // 29+ is 20ms
+};
+
+const ROTATION = {
+    LEFT: 'left',
+    RIGHT: 'right'
+};
+
+[COLORS, SHAPES, KEY, POINTS, LEVEL, ROTATION].forEach(item => Object.freeze(item));
