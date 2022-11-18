db4113ee41b631f8644558e06290bbc630100814
SurajPatel <Surajpatel527@gmail.com>
2022-11-17 19:28:48 -0500

    update constant

---
+++ tetris/constant.js
@@ -0,0 +1,17 @@
+'use scrict';
+
+const COLS = 10;
+const ROWS = 20;
+const BlOCK_SIZE = 30;
+const LINES_PER_LEVEL = 10;
+const NO_OF_HIGH_SCORES = 10;
+const COLORS = [
+    'none',
+    'cyan',
+    'blue',
+    'orange',
+    'yellow',
+    'green',
+    'purple',
+    'red',
+];
